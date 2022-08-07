410bd5593a7078ec72e9bce387cdb4f8725011db
Ivan Mendoza Garcia <ivan6361e@gmail.com>
2022-08-07 15:16:38 -0500

    first commit

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import App from "./App";
 import React from "react";
 import ReactDOM from "react-dom/client";
+import Axios from "axios";
+
+Axios.defaults.baseURL='https://nodejs-searchbar-rickandmorty.herokuapp.com'
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<App />);
